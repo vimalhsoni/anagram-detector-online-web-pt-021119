e9449bf8c0363de045b2b5cd13c901b1a0b2ae74
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-17 19:47:07 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+
+def match
+  new_array = @words.split('').sort
+  # new_array == words
+end
+
+
+end
