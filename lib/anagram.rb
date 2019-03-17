f6778c3f302dde8acef713178785e43ec3dbf84f
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-17 20:51:50 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+def match(test_array)
+  test_array.select {|word| word.split('').sort == @words.split('').sort}
+end
+
+end
